52b4229195c2247ec9604071903dc4d4b070db59
Mohammed <muji-amir@hotmail.com>
2023-12-18 20:48:10 -0500

    added some comments

--- phone.js
+++ phone.js
@@ -522,7 +522,7 @@ mouse.y = -(event.offsetY / canvas.clientHeight) * 2 + 1;
     }
 } 
 
-
+// Debug function that was used to close content info columns and set back to main phone model
 document.addEventListener('keydown', function(event) {
     // Check if Ctrl (or Command on Mac) and 'Z' key are pressed
     if (zoomedin) { 
@@ -558,6 +558,7 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+// main model loader that loads navigation and phone model as well as animation
 let loader = new GLTFLoader();
 loader.load('models/iphone12_less_parts/iphone_explosion.glb', function(gltf){
     model = gltf.scene;
@@ -601,11 +602,6 @@ function animate() {
 
 renderer.setAnimationLoop(animate);
 
-// window.addEventListener('resize', function() {
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-// })
 
 window.addEventListener('resize', () => {
     const newWidth = canvasContainer.clientWidth;
@@ -615,57 +611,3 @@ window.addEventListener('resize', () => {
     camera.aspect = newWidth / newHeight;
     camera.updateProjectionMatrix();
 });
-// let scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xdddddd);
-//
-// let camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-// camera.rotation.y = 45/180*Math.PI;
-// camera.position.x = 1100;
-// camera.position.y = 400;
-// camera.position.z = 1100;
-//
-// let renderer = new THREE.WebGLRenderer({antialias:true});
-// renderer.setSize(window.innerWidth,window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-//
-// let hlight = new THREE.AmbientLight (0x404040,100);
-// scene.add(hlight);
-//
-// let directionalLight = new THREE.DirectionalLight(0xffffff,100);
-// directionalLight.position.set(0,1,0);
-// directionalLight.castShadow = true;
-// scene.add(directionalLight);
-//
-// let light = new THREE.PointLight(0xc4c4c4,10);
-// light.position.set(0,300,500);
-// scene.add(light);
-//
-// let light2 = new THREE.PointLight(0xc4c4c4,10);
-// light2.position.set(500,100,0);
-// scene.add(light2);
-//
-// let light3 = new THREE.PointLight(0xc4c4c4,10);
-// light3.position.set(0,100,-500);
-// scene.add(light3);
-//
-// let light4 = new THREE.PointLight(0xc4c4c4,10);
-// light4.position.set(-500,300,500);
-// scene.add(light4);
-//
-// function animate() {
-//     renderer.render(scene,camera);
-//     requestAnimationFrame(animate);
-//     car.rotation.z += 0.01;
-// }
-//
-// let loader = new GLTFLoader();
-// let car;
-// loader.load('models/testing/test.glb', function(gltf){
-//   car = gltf.scene.children[0];
-//   console.log(gltf.scene.children.length);
-//
-//   car.scale.set(4600,4600,4600);
-//   if (car) car.rotation.y += 0.01;
-//   scene.add(gltf.scene);
-//   animate();
-// });
